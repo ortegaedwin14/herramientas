b65ff7d3aabdc052f559e6238204d7ac7301d77c
ortegaedwin14 <148915163+ortegaedwin14@users.noreply.github.com>
2025-04-22 09:20:44 -0500

    respuesta json

--- backend/index.js
+++ backend/index.js
@@ -4,13 +4,11 @@ const app = express(); // cargar en la constante todas las funcionalidades de ex
 
 app.use(cors()); // habilitar CORS para todas las solicitudes
 
-app.get('/', (req, res) => { // rol a ejecutarse
-  res.send('¡Hola, Mundo!');
+
+app.get('/pong', (req, res) => { // nuevo método GET
+  res.json({ message: 'Pong_Ortega' }); // envía una respuesta como objeto JSON
 });
 
-app.get('/ping', (req, res) => { // nuevo método GET
-  res.send('Pong'); // envía una respuesta en texto plano directamente
-});
 
 app.listen(3000, () => {
   console.log('Servidor corriendo en http://localhost:3000/');
---
+++ frontend/hola/readme.rd
@@ -0,0 +1,12 @@
+npm create vite@latest nombre_proyecto
+cd nombre_proyecto
+
+npm install
+
+npm run dev.
+
+-crear un input con un id
+- crear la funcion getPingFromWebService()
+- crear la constante que almacena la url del webService
+- obtener los datos y parsear la respuesta json
+- mostra un mensaje de erro si no funciona la url
