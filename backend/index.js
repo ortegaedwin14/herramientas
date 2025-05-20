51b21beac84a3096099659375ace478668a23bbf
ortegaedwin14 <148915163+ortegaedwin14@users.noreply.github.com>
2025-05-20 10:37:39 -0500

    hola

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,10 @@ app.get('/pong', (req, res) => { // nuevo método GET
   res.json({ message: 'Pong_Ortega' }); // envía una respuesta como objeto JSON
 });
 
+app.get('/saludar', (req, res) => {
+  res.send('hola mundo');
+});
+
 
 app.listen(3000, () => {
   console.log('Servidor corriendo en http://localhost:3000/');
